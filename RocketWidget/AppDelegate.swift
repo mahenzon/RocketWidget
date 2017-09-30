ed51c9a47f51e69bbd00264f63534f499bffa1a0
Suren Khorenyan <surenkhorenyan@gmail.com>
2017-09-30 19:53:38 +0300

    Initial Commit

---
+++ RocketWidget/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RocketWidget
+//
+//  Created by Suren Khorenyan on 30.09.17.
+//  Copyright © 2017 Suren Khorenyan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RocketWidget/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RocketWidget
+//
+//  Created by Suren Khorenyan on 30.09.17.
+//  Copyright © 2017 Suren Khorenyan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
