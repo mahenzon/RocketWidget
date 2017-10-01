55bbddb90d50f511f6fd26276f715f1498477ad9
Suren Khorenyan <surenkhorenyan@gmail.com>
2017-10-01 11:05:06 +0300

    Added outlets of UI to the ViewController

--- RocketWidget/View Controllers/ViewController.swift
+++ RocketWidget/View Controllers/ViewController.swift
@@ -9,19 +9,28 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    var imageViews = [NSImageView]()
+    var textLabels = [NSTextField]()
+    
+    @IBOutlet weak var balanceLabel: NSTextField!
+    @IBOutlet weak var rocketrubleLabel: NSTextField!
+    @IBOutlet weak var remainingCashoutsLabel: NSTextField!
+    @IBOutlet weak var spinner: NSProgressIndicator!
+    @IBOutlet weak var refreshButton: NSButton!
+    @IBOutlet weak var firstImageView: NSImageView!
+    @IBOutlet weak var secondImageView: NSImageView!
+    @IBOutlet weak var thirdImageView: NSImageView!
+    @IBOutlet weak var firstTextLabel: NSTextField!
+    @IBOutlet weak var secondTextLabel: NSTextField!
+    @IBOutlet weak var thirdTextLabel: NSTextField!
+    
+    @IBAction func refreshClicked(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imageViews = [firstImageView, secondImageView, thirdImageView]
+        textLabels = [firstTextLabel, secondTextLabel, thirdTextLabel]
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
 
