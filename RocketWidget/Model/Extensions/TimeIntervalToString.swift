a6c3988150b82ee7a6dcc2da673b51dafcb3db4f
Suren Khorenyan <surenkhorenyan@gmail.com>
2017-10-01 17:00:08 +0300

    Added String extension of TimeInterval

---
+++ RocketWidget/Model/Extensions/TimeIntervalToString.swift
@@ -0,0 +1,15 @@
+//
+//  TimeIntervalToString.swift
+//  RocketWidget
+//
+//  Created by Suren Khorenyan on 01.10.17.
+//  Copyright © 2017 Suren Khorenyan. All rights reserved.
+//
+
+import Foundation
+
+extension TimeInterval {
+    var stringValue: String {
+        return "\(Int(self))"
+    }
+}
